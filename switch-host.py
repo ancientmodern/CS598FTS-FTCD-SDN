c96b1d96e50983b60006eecb7fd424cdb10477de
ancientmodern <ancientmodern4@gmail.com>
2023-04-08 02:34:32 -0600

    test flow_table=0

--- switch-host.py
+++ switch-host.py
@@ -11,13 +11,13 @@ from mininet.topolib import TreeTopo
 from mininet.log import setLogLevel
 from mininet.cli import CLI
 
-setLogLevel( 'info' )
+setLogLevel('info')
 
 # Two local and one "external" controller (which is actually c0)
 # Ignore the warning message that the remote isn't (yet) running
-c1 = RemoteController( 'c1', ip='10.10.1.4', port=6633 )
-c2 = RemoteController( 'c2', ip='10.10.1.5', port=6633 )
-c3 = RemoteController( 'c3', ip='10.10.1.3', port=6633 )
+c1 = RemoteController('c1', ip='10.10.1.4', port=6633)
+c2 = RemoteController('c2', ip='10.10.1.5', port=6633)
+c3 = RemoteController('c3', ip='10.10.1.3', port=6633)
 # c4 = RemoteController( 'c4', ip='10.10.1.2', port=6633 )
 # c5 = RemoteController( 'c5', ip='10.10.1.6', port=6633 )
 
@@ -25,17 +25,18 @@ c3 = RemoteController( 'c3', ip='10.10.1.3', port=6633 )
 cmap = {'s1': [c1], 's2': [c2], 's3': [c3]}
 
 
-class MultiSwitch( OVSSwitch ):
+class MultiSwitch(OVSSwitch):
     "Custom Switch() subclass that connects to different controllers"
-    def start( self, controllers ):
-        return OVSSwitch.start( self, cmap[ self.name ] )
+
+    def start(self, controllers):
+        return OVSSwitch.start(self, cmap[self.name], protocols='OpenFlow13', flow_table=0)
 
 
-topo = TreeTopo( depth=2, fanout=2 )
-net = Mininet( topo=topo, switch=MultiSwitch, build=False, waitConnected=True )
-for c in [c1,c2,c3]:
+topo = TreeTopo(depth=2, fanout=2)
+net = Mininet(topo=topo, switch=MultiSwitch, build=False, waitConnected=True)
+for c in [c1, c2, c3]:
     net.addController(c)
 net.build()
 net.start()
-CLI( net )
+CLI(net)
 net.stop()
