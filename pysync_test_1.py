f95737d6d28992371ef439137535b4c5b4e8d153
Lan Zhang <lanzhgx@gmail.com>
2023-04-07 20:02:57 -0500

    debug

---
+++ pysync_test_1.py
@@ -0,0 +1,40 @@
+from pysyncobj import SyncObj, replicated
+
+class kvStore(SyncObj):
+    def __init__(self):
+        super(kvStore, self).__init__('10.10.1.4:9000', ['10.10.1.5:9000'])
+        self.mac_to_port = {}
+    
+    @replicated
+    def setDefault(self, dpid):
+        self.mac_to_port.setdefault(dpid, {})
+    
+    @replicated
+    def write(self, dpid, src, in_port):
+        self.mac_to_port[dpid][src] = in_port
+    
+    def read(self, dpid, dst):
+        return self.mac_to_port[dpid][dst]
+    
+    def isIn(self, dpid, dst):
+        return dst in self.mac_to_port[dpid]
+
+    def printAll(self):
+        print("in print all")
+        print(self.mac_to_port)
+
+class caller:
+    def __init__(self):
+        self.kvStore = kvStore()
+    
+    def add(self, dpid, src, in_port):
+        self.kvStore.setDefault(dpid)
+        self.kvStore.write(dpid, src, in_port)
+
+    def print(self):
+        self.kvStore.printAll()
+    
+myCaller = caller()
+myCaller.add(1, 1, 1)
+myCaller.add(1, 2, 2)
+myCaller.print()
