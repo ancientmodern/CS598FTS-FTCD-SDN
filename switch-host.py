b2b32b0d7f4200d514730d1d8ccff6f49a03c18d
Lan Zhang <lanzhgx@gmail.com>
2023-04-09 18:08:39 -0500

    add change controllers

--- switch-host.py
+++ switch-host.py
@@ -56,7 +56,7 @@ class MultiSwitch(OVSSwitch):
                         cmap[self.name] = newCtl
                         self.vsctl('set-controller', self.name, 'tcp:{}:{}'.format(newCtl.ip, newCtl.port))
                         sleep_cnt = 0
-                        while self.controllerUUIDs() == []:
+                        while self.controllerUUIDs() == uuids:
                             print(sleep_cnt)
                             sleep_cnt += 1
                             time.sleep(1)
