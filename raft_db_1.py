d2c0b49a8f97d83bcca34aaad7485363918ceff3
jiaaixu <jiaaixu@umich.edu>
2023-04-28 17:11:50 -0500

    modify raft socket

--- raft_db_1.py
+++ raft_db_1.py
@@ -1,51 +1,70 @@
 import socket
+import os
+import binascii
 from pysyncobj import SyncObj
 from mydict import ReplDict
-import sys
-
-mac_to_port = ReplDict()
-syncObj = SyncObj(
-    "node-1:9000", ["node-2:9000", "node-3:9000"], consumers=[mac_to_port]
-)
 
 
-def send_msg(sock, msg):
-    # Prefix each message with a 4-byte length (network byte order)
-    print("send msg:", msg)
-    sock.sendall(msg)
+def decode_mac_address(mac_bytes):
+    hex_mac = binascii.hexlify(mac_bytes).decode()
+    return ":".join(hex_mac[i : i + 2] for i in range(0, 12, 2))
 
 
-def recvall(sock, n):
-    data = bytearray(n)
-    while len(data) < n:
-        packet = sock.recv(n - len(data))
-        if not packet:
-            return None
-        data.extend(packet)
-    return data
+class SimpleServer:
+    def __init__(self, server_addr):
+        self.mac_to_port = ReplDict()
+        syncObj = SyncObj(
+            "node-1:9000", ["node-2:9000", "node-3:9000"], consumers=[self.mac_to_port]
+        )
+        self.server_address = server_addr
+        self.running = True
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        # Clean up the socket file if it exists
+        try:
+            os.unlink(self.server_address)
+        except OSError:
+            if os.path.exists(self.server_address):
+                raise
+
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.bind(self.server_address)
+
+        sock.listen(1)
+        print(f"Listen on {self.server_address}")
+
+        while self.running:
+            connection, _ = sock.accept()
+            response = connection.recv(10)
+            set_byte = response[0]
+            dpid = int.from_bytes(response[1:3], "big")  # only for print
+            mac_address = decode_mac_address(response[3:9])  # only for print
+            val = response[9]
+            key = response[1:9]  # Directly use bytearrays as keys for efficiency
+
+            if set_byte == 0x00:
+                # Get request
+                val = self.mac_to_port.get(key)
+                print(
+                    f"GET: dpid = {dpid}, mac_address = {mac_address}, get_val: {val}"
+                )
+                val = val if val is not None else 0xFF  # 0xFF means key does not exist
+                connection.send(val.to_bytes(1, "big"))
+            else:
+                print(
+                    f"SET: dpid = {dpid}, mac_address = {mac_address}, set_val: {val}"
+                )
+                self.mac_to_port.set(key, val)
+                # self.mac_to_port[key] = val
+
+            connection.close()
+
+        sock.close()
 
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to a local address and port
-# server_address = ('localhost', 12345)
-server_address = "/tmp/sdn-uds.sock"
-server_socket.bind(server_address)
-server_socket.listen(1)
-client_socket, address = server_socket.accept()
-while True:
-    print("Waiting for incoming connections...")
-    data = recvall(client_socket, 10)
-    print("Connected to", client_socket)
-    # get request
-    if data[0] == 0x00:
-        key = bytes(data[1:9])
-        val = mac_to_port.get(key)
-        print("get val:", val)
-        print("socket:", client_socket)
-        send_msg(client_socket, val)
-    # set request
-    elif data[0] == 0x01:
-        key = bytes(data[1:9])
-        val = data[9]
-        print("set val:", val)
-        mac_to_port.set(key, val)
+if __name__ == "__main__":
+    server = SimpleServer("/tmp/sdn_uds.sock")
+    server.run()
