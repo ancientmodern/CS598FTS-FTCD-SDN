a1550cfc6c68ce1b85c3b950e8509f2143d18f5e
ancientmodern <ancientmodern4@gmail.com>
2023-04-29 18:42:13 -0600

    set controller flow to 100 after taking over

--- switch-host-takeover.py
+++ switch-host-takeover.py
@@ -14,18 +14,18 @@ import time
 import random
 from threading import Thread, Event
 
-setLogLevel('info')
+setLogLevel("info")
 
 # Two local and one "external" controller (which is actually c0)
 # Ignore the warning message that the remote isn't (yet) running
-c1 = RemoteController('c1', ip='10.10.1.4', port=6633)
-c2 = RemoteController('c2', ip='10.10.1.5', port=6633)
-c3 = RemoteController('c3', ip='10.10.1.3', port=6633)
+c1 = RemoteController("c1", ip="10.10.1.4", port=6633)
+c2 = RemoteController("c2", ip="10.10.1.5", port=6633)
+c3 = RemoteController("c3", ip="10.10.1.3", port=6633)
 # c4 = RemoteController( 'c4', ip='10.10.1.2', port=6633 )
 # c5 = RemoteController( 'c5', ip='10.10.1.6', port=6633 )
 
 # cmap = { 's1': [c1,c2,c3,c4,c5], 's2': [c1,c2,c3,c4,c5], 's3': [c1,c2,c3,c4,c5], 's4': [c1,c2,c3,c4,c5], 's5': [c1,c2,c3,c4,c5], 's6': [c1,c2,c3,c4,c5] }
-cmap = {'s1': c1, 's2': c2, 's3': c3}
+cmap = {"s1": c1, "s2": c2, "s3": c3}
 onlineControllers = {c1, c2, c3}
 
 
@@ -40,29 +40,48 @@ class MultiSwitch(OVSSwitch):
             while not stop_event.is_set():
                 # isc = self.connected()
                 isc = False
-                uuid = self.vsctl('-- get Bridge', self, 'Controller').strip()
+                uuid = self.vsctl("-- get Bridge", self, "Controller").strip()
                 uuid = uuid[1:-1]
-                res = self.vsctl('-- get Controller', uuid, 'is_connected')
-                if 'true' in res:
+                res = self.vsctl("-- get Controller", uuid, "is_connected")
+                if "true" in res:
                     isc = True
 
                 if not isc:
-                    self.vsctl('del-controller', self.name)
+                    self.vsctl("del-controller", self.name)
                     if cmap[self.name] in onlineControllers:
                         onlineControllers.remove(cmap[self.name])
                     newCtl = random.choice(list(onlineControllers))
                     cmap[self.name] = newCtl
-                    self.vsctl('set-controller', self.name, 'tcp:{}:{}'.format(newCtl.ip, newCtl.port))
+                    self.vsctl(
+                        "set-controller",
+                        self.name,
+                        "tcp:{}:{}".format(newCtl.ip, newCtl.port),
+                    )
+                    self.dpctl(
+                        "add-flow",
+                        "cookie=0x0,table=0,priority=100,actions=CONTROLLER:65535",
+                    )
 
                     sleep_cnt = 0
-                    new_uuid = self.vsctl('-- get Bridge', self, 'Controller').strip()[1:-1]
-                    new_res = self.vsctl('-- get Controller', new_uuid, 'is_connected')
-                    while not (new_uuid != uuid and 'true' in new_res):
+                    new_uuid = self.vsctl("-- get Bridge", self, "Controller").strip()[
+                        1:-1
+                    ]
+                    new_res = self.vsctl("-- get Controller", new_uuid, "is_connected")
+                    while not (new_uuid != uuid and "true" in new_res):
                         print(sleep_cnt, new_uuid, new_res)
                         sleep_cnt += 1
                         time.sleep(1)
-                        new_uuid = self.vsctl('-- get Bridge', self, 'Controller').strip()[1:-1]
-                        new_res = self.vsctl('-- get Controller', new_uuid, 'is_connected')
+                        new_uuid = self.vsctl(
+                            "-- get Bridge", self, "Controller"
+                        ).strip()[1:-1]
+                        new_res = self.vsctl(
+                            "-- get Controller", new_uuid, "is_connected"
+                        )
+
+                    self.dpctl(
+                        "add-flow",
+                        "cookie=0x0,table=0,priority=100,actions=CONTROLLER:65535",
+                    )
 
         monitor_thread = Thread(target=isConnected, args=(self.stop_event,))
         monitor_thread.daemon = True
@@ -84,9 +103,9 @@ net.start()
 # Modify flow table entries after the network has started
 for switch in net.switches:
     # Delete all flow table entries
-    switch.dpctl('del-flows')
+    switch.dpctl("del-flows")
     # Set the priority of the controller flow entry to 100, ensuring that all flows are directed to the controller
-    switch.dpctl('add-flow', 'cookie=0x0,table=0,priority=100,actions=CONTROLLER:65535')
+    switch.dpctl("add-flow", "cookie=0x0,table=0,priority=100,actions=CONTROLLER:65535")
 
 CLI(net)
 net.stop()
