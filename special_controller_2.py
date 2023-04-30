c84b49af311a60e24bfacdee76d2df6821cd5e10
ancientmodern <ancientmodern4@gmail.com>
2023-04-29 19:30:46 -0600

    update special controller

--- special_controller_2.py
+++ special_controller_2.py
@@ -143,9 +143,8 @@ class SimpleSwitch13(app_manager.RyuApp):
         if out_port is None:
             out_port = ofproto.OFPP_FLOOD
 
-        if dpid == 1 and self.count < 300:
+        if dpid <= 21 and self.count < 6000:
             out_port = ofproto.OFPP_FLOOD
-            time.sleep(0.01)
             self.count += 1
             self.logger.info(
                 "do not learn %d %s %s %s, as count = %d",
